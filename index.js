e44b202a81a1e1d3208bb419d988be71178b41b5
Ms-Waiyego <waiyegobrenda99@gmail.com>
2022-09-05 21:01:00 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(userName,userEmail){
+    let userObject = {
+        name : userName,
+        email : userEmail
+    };
+    let userConfig = {
+        method : "POST",
+        headers : {
+            "Content-type" : "application/json",
+            Accept : "application/json"
+        },
+        body : JSON.stringify(userObject)
+    }
+    let postUser = fetch("http://localhost:3000/users",userConfig)
+                    .then(res => res.json())
+                    .then(object => {
+                        document.body.append(object.id)
+                    })
+                    .catch(err => {
+                        document.body.append(err.message);
+                    })
+    return postUser;             
+}
